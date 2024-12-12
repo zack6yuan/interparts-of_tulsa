3a45e68fdd83c271c16436cf6316aef4f3efd9f9
zack6yuan <zack6yuan@gmail.com>
2024-12-11 19:56:26 -0600

    upload logo and update app route.

--- app.py
+++ app.py
@@ -10,4 +10,26 @@ def home():
   return render_template('index.html')
 
 
-@app.route()
+@app.route('/specials', strict_slashes=False)
+def specials():
+  return render_template(specials.html)
+
+
+@app.route('/reviews', strict_slashes=False)
+def reviews():
+  return render_template(reviews.html)
+
+
+@app.route('/about', strict_slashes=False)
+def about():
+  return render_template(about.html)
+
+
+@app.route('/gallery', strict_slashes=False)
+def gallery():
+  return render_template(gallery.html)
+
+
+@app.route('/contact', strict_slashes=False)
+def contact():
+  return render_template(contact.html)
